583ae1ad20cbf514645f603872e0d4ce516d9fab
Aniket Kumar Singh <aniketks321@gmail.com>
2021-06-26 14:44:22 +0530

    THA_Day9 completed file Uploaded.

---
+++ THA_Day9/script/script.js
@@ -0,0 +1,23 @@
+var bookedSeatCount = document.querySelector(".booked-seats-count");
+var availableSeatCount = document.querySelector(".available-seats-count");
+var booked = 0;
+var available = 36;
+const blocks = document.querySelectorAll(".blocks");
+
+blocks.forEach((block) => {
+    block.addEventListener("click", (e)=>{
+        if (e.target.classList.contains("booked")) {
+            e.target.classList.remove("booked");
+            booked--;
+            available++;
+            bookedSeatCount.innerHTML = `${booked}`;
+            availableSeatCount.innerHTML = `${available}`;
+        }else {
+            e.target.classList.add("booked");
+            booked++;
+            available--;
+            bookedSeatCount.innerHTML = `${booked}`;
+            availableSeatCount.innerHTML = `${available}`;
+        }
+    })
+})
