aecb450f27bfa4c88ee95116ac7332db17ee2747
Aniket Kumar Singh <aniketks321@gmail.com>
2021-07-08 20:31:23 +0530

    Meme Card code uploaded.

---
+++ THA_Day16/first-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from './Components/Card'
+import './index.css'
+
+function App() {
+    return (
+        <div className="App">
+            <Card />
+        </div>
+    );
+}
+
+export default App;
---
+++ THA_Day16/first-app/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <img src="https://i.redd.it/ks9boperc3471.jpg" alt="Meme" />
+            <h2>Meme Card!</h2>
+            <p>This is a Meme Card</p>
+        </div>
+    );
+}
+
+export default Card;
